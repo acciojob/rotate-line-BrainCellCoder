0e877a92d7aa9932791c9ab5772f1b8de0618804
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-23 22:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const line = document.getElementById('line');
+    let angle = 0;
+
+    function rotateLine() {
+        angle = (angle + 2) % 360;
+        line.style.transform = `rotate(${angle}deg)`;
+    }
+
+    setInterval(rotateLine, 20);
+});
